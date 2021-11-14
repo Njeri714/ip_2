bcc536746f20d92ab1b0fefe92f33eec697f0e72
njeri waweru <wawerunjeri714@gmail.com>
2021-11-14 17:27:57 +0300

    had function in js plus two variables

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
  let maleAkanNames = ['Kiwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+
  let femaleAkanNames = ['Akasua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+
  let dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+ 
+ function akangenerate(){
+     let dateNumber = document.getElementById('date').value
+     let gender = document.getElementById('gender').value
+    }
