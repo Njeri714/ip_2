8a6fcdbd15be25e7a6a84349de78df513b56efeb
njeri waweru <wawerunjeri714@gmail.com>
2021-11-14 17:44:53 +0300

    added if statement

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
- let maleAkanNames = ['Kiwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+let maleAkanNames = ['Kiwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
 
- let femaleAkanNames = ['Akasua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+let femaleAkanNames = ['Akasua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 
- let dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
- 
- function akangenerate(){
-     let dateNumber = document.getElementById('date').value
-     let gender = document.getElementById('gender').value
-     let dayNumber = new date (date).getday
+let dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+function akangenerate() {
+    let dateNumber = document.getElementById('date').value
+    let gender = document.getElementById('gender').value
+
+    let dayNumber = new Date(date).getday()
+
+    if (gender == 'female') {
+        console.log(`you were born on ${daysOfWeek[dayNumber]} and your Akan name is ${femaleAkanNames[dayNumber]}`)
+
     }
+    else if (gender == 'male'){
+        console.log(``)
+        
+    }
+
+
+
+
+
+
+
+
+
+}
